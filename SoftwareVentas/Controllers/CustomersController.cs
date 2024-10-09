10d20a47ada77c83a29b6845dffc7d5e7163e537
jonathanramirez253631 <jonathanramirez253631@correo.itm.edu.co>
2024-10-09 14:26:17 -0500

    se agrego edit a customer

--- SoftwareVentas/Controllers/CustomersController.cs
+++ SoftwareVentas/Controllers/CustomersController.cs
@@ -54,30 +54,50 @@ namespace SoftwareVentas.Controllers
         }
 
         //Here we look for the client id so we can move on to the edit view
-        [HttpGet]
-        public async Task<IActionResult> Edit(int id)
-        {
-            var customer = await _context.Customers.FindAsync(id);
-            if (customer == null)
-            {
-                return RedirectToAction(nameof(Index));
-            }
-
-            return View(customer);
-        }
-
-        //Here we verify that it is valid to be able to perform the update
         [HttpPost]
         public async Task<IActionResult> Edit(Customer customer)
         {
-            if (!ModelState.IsValid)
+            try
             {
-                return View(customer);
-            }
+                if (!ModelState.IsValid)
+                {
+                    return View(customer);
+                }
+                
+                _context.Customers.Update(customer);
 
-            _context.Customers.Update(customer);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+                               
+            }
+            catch (Exception ex)
+            {
+               // Console.WriteLine(ex.Message);
+                //throw;
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        //Here we verify that it is valid to be able to perform the update
+        [HttpGet]
+        public async Task<IActionResult> Edit([FromRoute] int id)
+        {
+            try
+            {
+                Customer? customer= await _context.Customers.FirstOrDefaultAsync(a => a.idCustomer == id);
+
+
+                return View(customer);
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+                //return RedirectToAction(nameof(Index));
+            }
         }
 
         // Here what we do is find a client by the id and delete it
