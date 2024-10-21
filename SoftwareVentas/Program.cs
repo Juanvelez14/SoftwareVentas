9e9b5563cd4753b8497a4d20b5dedf35a4b745df
jonathanramirez253631 <jonathanramirez253631@correo.itm.edu.co>
2024-10-21 15:49:59 -0500

    llamo al home desde el program principal

--- SoftwareVentas/Program.cs
+++ SoftwareVentas/Program.cs
@@ -33,7 +33,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Products}/{action=Index}/{id?}");
+   pattern: "{controller=Home}/{action=Index}/{id?}");
 
     //pattern: "{controller=Customers}/{action=Index}/{id?}");
 
