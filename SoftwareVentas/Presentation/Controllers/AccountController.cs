ecd32103816cda7735737a35fe74f7ef28c39522
Miguel Betancur <miguelangel.bet0211@gmail.com>
2024-11-10 13:10:47 -0500

    Cambios en el backend para roles y permisos

---
+++ SoftwareVentas/Presentation/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using SoftwareVentas.BLL;
+
+using SoftwareVentas.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SoftwareVentas.Presentation.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginDTO dto)
+        {
+            return View();
+        }
+    }
+}
